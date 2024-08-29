f421099bb2f9d4428ddab2c501d6fb1658597fd1
Logan Wyatt <loganwyatt1995@gmail.com>
2024-08-29 19:35:37 +0000

    Setup for laps

---
+++ getline/laps.c
@@ -0,0 +1,13 @@
+#include "laps.h"
+
+/**
+ * race_state - keeps track of a race between several cars
+ * @id: pointer to an array of car identifiers
+ * @size: size of array for id
+ *
+ * Return: n/a
+**/
+
+void race_state(int *id, size_t size)
+{
+
---
+++ getline/laps.h
@@ -0,0 +1,6 @@
+#ifndef laps.h
+#define laps.h
+
+void race_state(int *id, size_t size)
+
+#endif
