3224cea495a8b2c8ffa5ed5beea4fbfa5be3a1f0
Logan Wyatt <loganwyatt1995@gmail.com>
2024-08-31 20:10:07 +0000

    Add race car tracker

--- getline/laps.c
+++ getline/laps.c
@@ -1,4 +1,74 @@
 #include "laps.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+
+
+/**
+ * create_car - creates a car_node in BST
+ * @car_id - unique id for car node
+ *
+ * Return - pointer to new node
+**/
+
+car_node_t *create_car(int car_id)
+{
+	car_node_t *new_car;
+
+	new_car = malloc(sizeof(car_node_t));
+	if (!new_car)
+		return (NULL);
+	new_car->car_id = car_id;
+	new_car->lap_num = 0;
+	new_car->car_left = new_car->car_right = NULL;
+	printf("Car %d joined the race\n", car_id);
+	return new_car;
+}
+
+/**
+ * car_update - checks each id and updates lap if exists,
+ *				otherwise adds id to BST
+ * @car_id: identification of car
+ * @root: pointer to root of BST
+ *
+ * Return: n/a
+**/
+
+car_node_t *car_update(int car_id, car_node_t *root)
+{
+	if (root == NULL)
+		return create_car(car_id);
+	
+	if (car_id == root->car_id)
+	{
+		root->lap_num++;
+		return root;
+	}
+	else if (car_id < root->car_id)
+		root->car_left = car_update(car_id, root->car_left);
+	else if (car_id > root->car_id)
+		root->car_right = car_update(car_id, root->car_right);
+	
+	return root;
+}
+
+/**
+ * print_race - prints all cars and laps
+ * @root: pointer to car
+ *
+ * Return: n/a
+**/
+
+void print_race(car_node_t *root)
+{
+	if (root != NULL)
+	{
+		print_race(root->car_left);
+		printf("Car %d [%d laps]\n", root->car_id, root->lap_num);
+		print_race(root->car_right);
+	}
+}
 
 /**
  * race_state - keeps track of a race between several cars
@@ -10,4 +80,19 @@
 
 void race_state(int *id, size_t size)
 {
+	size_t i = 0;
+	static car_node_t *root;
 
+	if (size == 0)
+	{
+		/* free everything */
+	}
+
+	/* iterate through id list */
+	for (i = 0; i < size; i++)
+	{
+		root = car_update(id[i], root);
+	}
+	printf("Race state:\n");
+	print_race(root);
+}
--- getline/laps.h
+++ getline/laps.h
@@ -1,6 +1,35 @@
-#ifndef laps.h
-#define laps.h
+#include <stddef.h>
 
-void race_state(int *id, size_t size)
+#ifndef LAPS_H
+#define LAPS_H
+
+/* Struct */
+
+/**
+ * struct carnode_s - node in binary search tree
+ * @car_id: identification for car node
+ * @lap_num: number of laps car has done
+ * @car_left: pointer to left child/node
+ * @car_right: pointer to right child/node
+ *
+ * Description: This struct is to be used in conjuction with race_state.
+ *              It will store information about each car in a race and
+ *              what lap said car is on.
+**/
+
+typedef struct carnode_s
+{
+	int car_id;
+	int lap_num;
+	struct carnode_s *car_left;
+	struct carnode_s *car_right;
+} car_node_t;
+
+/* Prototypes */
+
+void race_state(int *id, size_t size);
+car_node_t *create_car(int car_id);
+car_node_t *car_update(int car_id, car_node_t *root);
+void print_race(car_node_t *root);
 
 #endif
