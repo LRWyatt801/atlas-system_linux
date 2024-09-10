283719e8cadf7a2f71b81f3970051b400ef83e9f
Logan Wyatt <loganwyatt1995@gmail.com>
2024-09-10 02:01:06 +0000

    simple ls print needs some work

---
+++ ls/list.h
@@ -0,0 +1,38 @@
+#ifndef _LIST_H
+#define _LIST_H
+
+#include <dirent.h>
+
+/****** STRUCTS ******/
+
+/**
+ * dir_lister_s - Iterator for parsing 'DIR' stream
+ * @directory: Pointer to 'DIR' instance
+ * @path: Path to directory in file system
+ * @current_entry: Pointer to current 'struct dirent'
+ * @stop: int flag to indicate directory entries have been exhausted
+**/
+
+typedef struct dir_lister_s
+{
+	DIR *directory;
+	const char *path;
+	struct dirent *current_entry;
+	int stop;
+} dir_lister_t;
+
+/****** PROTOTYPES ******/
+
+/* LIST_MAIN */
+
+int main(int argc, char **argv);
+
+/* LISTER */
+
+int directory_lister_init(dir_lister_t *parser,
+						 const char *path);
+int print_dir(dir_lister_t *parser);
+struct dirent *next_entry(dir_lister_t *parser);
+void close_dir(dir_lister_t *parser);
+
+#endif
---
+++ ls/lister.c
@@ -0,0 +1,73 @@
+#include "list.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int directory_lister_init(dir_lister_t *parser,
+						  const char *path)
+{
+	DIR *dir = NULL;
+
+	if (!parser || !path)
+		return(-1);
+
+	dir = opendir(path);
+
+	if (!dir)
+		return(-1);
+
+	parser->directory = dir;
+	parser->path = path;
+	parser->current_entry = NULL;
+	parser->stop = 0;
+
+	return(0);
+}
+
+int print_dir(dir_lister_t *parser)
+{
+	if (!parser)
+		return(-1);
+
+	while (next_entry(parser))
+	{
+		if (parser->stop == 0)
+			printf("%s ", parser->current_entry->d_name);
+		else
+			printf("%s\n", parser->current_entry->d_name);
+	}
+
+	return(0);
+}
+
+struct dirent *next_entry(dir_lister_t *parser)
+{
+	struct dirent *next_entry = NULL;
+
+	if (parser->stop)
+		return(NULL);
+
+	next_entry = readdir(parser->directory);
+
+	if (next_entry)
+		parser->current_entry = next_entry;
+	else
+	{
+		parser->current_entry = NULL;
+		parser->stop = 1;
+	}
+
+	return(next_entry);
+}
+
+void close_dir(dir_lister_t *parser)
+{
+	if (!parser)
+		return;
+
+	closedir(parser->directory);
+	memset(parser, 0, sizeof(*parser));
+	return;
+}
