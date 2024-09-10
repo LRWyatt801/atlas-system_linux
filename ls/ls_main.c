eddf4a9b4f6b4791c94aad199ea0e533df163e39
Logan Wyatt <loganwyatt1995@gmail.com>
2024-09-10 20:23:45 +0000

    Add path name input

--- ls/ls_main.c
+++ ls/ls_main.c
@@ -4,10 +4,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
     dir_lister_t parser;
-    const char *directory_path = ".";
+    const char *directory_path = NULL;
+
+    if (argc == 1)
+        directory_path = ".";
+    else
+        directory_path = argv[1];
 
     directory_lister_init(&parser, directory_path);
 
