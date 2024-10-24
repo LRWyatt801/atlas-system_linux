47639e945812093c919a83ca6ac1fae7d4829624
Logan Wyatt <loganwyatt1995@gmail.com>
2024-10-24 16:37:56 +0000

    strchr in assembly

---
+++ libasm/3-strchr.asm
@@ -0,0 +1,41 @@
+BITS 64
+
+section .data
+    ; No needed global vars
+
+section .text
+    global asm_strchr
+
+    ; char *asm_strchr(const char *s, int c);
+
+asm_strchr:
+    ; set up stack frame
+    push    rbp
+    mov     rbp, rsp
+
+    xor     rax, rax                    ; returning address needs rax
+    xor     rcx, rcx
+
+.loop:
+    ; move char to ax
+    movzx   eax, byte[rdi]
+
+    ; check for null byte
+    cmp     eax, 0
+    jz      .done
+    ; cmp char to int
+    cmp     eax, esi
+    je      .found
+    inc     rdi
+    jmp     .loop
+
+.found:
+    ; set eax to found char
+    mov     rax, rdi
+    jmp     .done
+
+.done:
+    ; break down stack frame
+    mov     rsp, rbp
+    pop     rbp
+    ret
--- libasm/libasm.h
+++ libasm/libasm.h
@@ -5,6 +5,7 @@
 
 /****** Prototypes ******/
 
+char *asm_strchr(const char *s, int c);
 int asm_strcmp(const char *s1, const char *s2);
 size_t asm_strlen(const char *str);
 int asm_strncmp(const char *s1, const char *s2, size_t n);
