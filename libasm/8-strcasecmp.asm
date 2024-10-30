e88f00d0b297a63d2e9f24b6124ec67e128de629
Logan Wyatt <loganwyatt1995@gmail.com>
2024-10-30 00:58:29 +0000

    Just to test against checker

--- libasm/8-strcasecmp.asm
+++ libasm/8-strcasecmp.asm
@@ -11,8 +11,46 @@ asm_strcasecmp:
     push    rbp
     mov     rbp, rsp
 
+    xor     rax, rax
 
+    ; check for null strs
+    cmp     rdi, 0
+    jz      .done
+    cmp     rsi, 0
+    jz      .done
 
+.cmp_loop:
+    mov     al, byte[rdi]
+    mov     bl, byte[rsi]
+
+    ; check for '\0'
+    cmp     al, 0
+    jz      .done
+    cmp     bl, 0
+    jz      .done
+
+    ; convert to lowercase
+    and     al, 0dfh
+    and     bl, 0dfh
+
+    cmp     al, bl
+    jl      .less_than
+    jg      .greater_than
+
+    ; move to next char
+    inc     rdi
+    inc     rsi
+    jmp     .cmp_loop
+
+.less_than:
+    mov     rax, -1
+    jmp     .done
+
+.greater_than:
+    mov     rax, 1
+    jmp     .done
+
+.done:
     ; breakdown stack frame
     mov     rsp, rbp
     pop     rbp
