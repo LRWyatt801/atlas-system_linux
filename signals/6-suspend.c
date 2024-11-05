85fb66fb1b7db9436ae1a6d60ca46795eceadbc1
Logan Wyatt <loganwyatt1995@gmail.com>
2024-11-05 02:32:20 +0000

    Suspend sigint

---
+++ signals/6-suspend.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+
+static volatile sig_atomic_t signal_recieved = 0;
+
+/**
+ * sigint_handler - handles the SIGINT signal
+ *
+ * @signum: signal number
+ */
+void sigint_handler(int signum)
+{
+	printf("Caught %d\n", signum);
+	signal_recieved = 1;
+}
+
+/**
+ * main - entry point
+ *
+ * Return: 0 on success, otherwise -1
+ */
+
+int main(void)
+{
+	struct sigaction act;
+
+	act.sa_handler = sigint_handler;
+	act.sa_flags = 0;
+	sigemptyset(&act.sa_mask);
+	if (sigaction(SIGINT, &act, NULL) == -1)
+		return (EXIT_FAILURE);
+	
+	pause();
+
+	if (signal_recieved)
+	{
+		printf("Signal received\n");
+		return (EXIT_SUCCESS);
+	}
+}
