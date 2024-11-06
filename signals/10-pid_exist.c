4d2c5d9bda963ecac88204b0c60249261eec4ac0
Logan Wyatt <loganwyatt1995@gmail.com>
2024-11-06 02:04:33 +0000

    Fix for Betty

--- signals/10-pid_exist.c
+++ signals/10-pid_exist.c
@@ -1,6 +1,13 @@
 #include <signal.h>
 
+/**
+ * pid_exist - check if a pid exist
+ * @pid: pid to check
+ *
+ * Return: 1 if exist, 0 if not
+ */
+
 int pid_exist(pid_t pid)
 {
-	return (kill(pid, 0) == 0? 1: 0);
+	return (kill(pid, 0) == 0 ? 1 : 0);
 }
