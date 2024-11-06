e6854bd56d6e9ec46bac42be97a47b0cbf5fdafd
Logan Wyatt <loganwyatt1995@gmail.com>
2024-11-06 01:19:58 +0000

    Send kill sigint

---
+++ signals/7-signal_send.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+
+/**
+ * main - Entry point for signal send
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * Return: 0 on success, 1 on failure
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		printf("Usage: %s <signum>\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
+
+	pid_t pid = (pid_t)atoi(argv[1]);
+
+	if (kill(pid, SIGINT) == -1)
+		return (EXIT_FAILURE);
+
+	return (EXIT_SUCCESS);
+}
