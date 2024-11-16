50ef421cf18f944b40b5fc6c4b294ce2476b0188
Logan Wyatt <loganwyatt1995@gmail.com>
2024-11-16 22:21:18 +0000

    Fix compile

--- 0x08_CPython/2-python.c
+++ 0x08_CPython/2-python.c
@@ -1,6 +1,9 @@
 #include "Python.h"
 #include <stdio.h>
 
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+
 /**
  * print_python_list - prints basic info about python lists
  * @p: PyObject
