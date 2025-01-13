2b72ed86140f4bd0bf713b14064c4a9d0602915b
LRWyatt801 <loganwyatt1995@gmail.com>
2025-01-13 13:21:02 -0600

    update for task 2

--- strace/strace_hdr.h
+++ strace/strace_hdr.h
@@ -10,5 +10,6 @@
 int child_function(char *, char **);
 int parent_function(pid_t);
 void get_syscall_name(unsigned long int);
+void get_syscall_info(unsigned long int);
 
 #endif /* STRACE_HDR_H */
